dac7ccf4ca594487f76b4f55e54242e524466f2f
sainath11-reddy <sainath11reddy@gmail.com>
2022-11-24 11:21:29 +0530

    Just for dun

--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const sequelize = require('sequelize');
 const Sequelize = new sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-    dialect:'mysql',
+    // dialect:'mysql',
     host:process.env.DB_HOST
 });
 
